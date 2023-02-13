9a2db24474592c5dd9c5f3bef8aa92c9a1a3a41d
Miguel Galperin <miguel.galperin@gmail.com>
2023-02-13 09:20:50 -0300

    navbar width

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -47,8 +47,8 @@ import { Button } from "@/components/ui/button"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full bg-dracula-aro-50 md:bg-transparent">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-2">
+    <header className="sticky top-0 z-40 max-w-[1320px] bg-dracula-aro-50 md:bg-transparent">
+      <div className="container flex h-16 items-center space-x-2 sm:justify-between sm:space-x-2">
         <MainNav items={siteConfig.mainNav}/>
         <Dropdown_menu/>
       
