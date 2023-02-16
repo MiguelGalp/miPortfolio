1f2c405089713e0de2cc22a14a401c4d1e799134
Miguel Galperin <miguel.galperin@gmail.com>
2023-02-16 12:03:54 -0300

    mockups mobile

--- components/dropdown_mobile.tsx
+++ components/dropdown_mobile.tsx
@@ -18,7 +18,11 @@ import {
     UserPlus,
     Users,
     CupSoda,
-    Clock
+    Clock,
+    LucideWifi,
+    Clapperboard,
+    HomeIcon,
+    ShoppingCart
   } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
@@ -62,29 +66,27 @@ export function Dropdown_mobile(){
         <DropdownMenuContent className="w-40">
           <DropdownMenuLabel className="flex">Mis trabajos</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          
-          <DropdownMenuSeparator />
           <DropdownMenuGroup>
             <DropdownMenuSub>
               <DropdownMenuSubTrigger>
-                <UserPlus className="mr-2 h-4 w-4" />
-                <span>Invite users</span>
+                <LucideWifi className="mr-2 h-4 w-4" />
+                <span>Web Atlanticx</span>
               </DropdownMenuSubTrigger>
               <DropdownMenuPortal className="">
                 <DropdownMenuSubContent className="">
                 <ul className="grid">
-                <li className="bg-cover bg-center" style={{backgroundImage: "url('/NuevoScreenShot_Atlanticx.png')"}}>
+                <li className="bg-cover bg-center" style={{backgroundImage: "url('/atx_mockup.png')"}}>
 
                 
                   <a
                     className="flex h-[400px] w-[200px] select-none flex-col justify-end rounded-md p-6 no-underline outline-none focus:shadow-md"
                     href="https://atlanticx.org"
                   >
-                    <Icons.logo className="h-6 w-6 text-slate-900" />
-                    <div className="mt-4 mb-2 text-lg font-medium text-slate-900">
+                    <Icons.logo className="h-6 w-6 text-slate-200" />
+                    <div className="mt-4 mb-2 text-lg font-medium text-slate-200">
                       Sitio Atlanticx
                     </div>
-                    <p className="text-sm leading-tight text-slate-900">
+                    <p className="text-sm leading-tight text-slate-200">
                       CMS desde Notion.so
                     </p>
                   </a>
@@ -95,11 +97,96 @@ export function Dropdown_mobile(){
                 </DropdownMenuSubContent>
               </DropdownMenuPortal>
             </DropdownMenuSub>
-            <DropdownMenuItem>
-              <Plus className="mr-2 h-4 w-4" />
-              <span>New Team</span>
-              <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-            </DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <Clapperboard className="mr-2 h-4 w-4" />
+                <span>Micro Saas</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal className="">
+                <DropdownMenuSubContent className="">
+                <ul className="grid">
+                <li className="bg-cover bg-center" style={{backgroundImage: "url('/miss_mockup.png')"}}>
+
+                
+                  <a
+                    className="flex h-[400px] w-[200px] select-none flex-col justify-end rounded-md p-6 no-underline outline-none focus:shadow-md"
+                    href="https://atlanticx.org"
+                  >
+                    <Icons.logo className="h-6 w-6 text-slate-800" />
+                    <div className="mt-4 mb-2 text-lg font-medium text-slate-800">
+                      Miss-subtitulos.app
+                    </div>
+                    <p className="text-sm leading-tight text-slate-900">
+                      Un micro servicio SaaS que utiliza IA para generar subtítulos
+                    </p>
+                  </a>
+                
+              </li>
+            
+            </ul>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <HomeIcon className="mr-2 h-4 w-4" />
+                <span>Casa Muro</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal className="">
+                <DropdownMenuSubContent className="">
+                <ul className="grid">
+                <li className="bg-cover bg-center" style={{backgroundImage: "url('/muro_mockup.png')"}}>
+
+                
+                  <a
+                    className="flex h-[400px] w-[200px] select-none flex-col justify-end rounded-md p-6 no-underline outline-none focus:shadow-md"
+                    href="https://atlanticx.org"
+                  >
+                   
+                    <div className="mt-4 mb-2 text-lg font-medium text-slate-900">
+                     
+                    </div>
+                    <p className="text-sm leading-tight text-slate-900">
+                      
+                    </p>
+                  </a>
+                
+              </li>
+            
+            </ul>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <ShoppingCart className="mr-2 h-4 w-4" />
+                <span>Tienda MP</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal className="">
+                <DropdownMenuSubContent className="">
+                <ul className="grid">
+                <li className="bg-cover bg-center" style={{backgroundImage: "url('/mockup_revista.png')"}}>
+
+                
+                  <a
+                    className="flex h-[400px] w-[200px] select-none flex-col justify-end rounded-md p-6 no-underline outline-none focus:shadow-md"
+                    href="https://atlanticx.org"
+                  >
+                    <Icons.logo className="h-6 w-6 text-slate-100" />
+                    <div className="mt-4 mb-2 text-lg font-medium text-slate-100">
+                      Revista y Shop
+                    </div>
+                    <p className="text-sm leading-tight text-slate-900">
+                      Web cultural, contenidos y radio en vivo
+                    </p>
+                  </a>
+                
+              </li>
+            
+            </ul>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
           </DropdownMenuGroup>
           <DropdownMenuSeparator />
           <DropdownMenuItem>
