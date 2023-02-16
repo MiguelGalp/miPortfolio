943554f89acd6a5ab5f844288f42159635bc8afe
Miguel Galperin <miguel.galperin@gmail.com>
2023-02-16 13:53:43 -0300

    cover sin gradient

--- components/cover_image.tsx
+++ components/cover_image.tsx
@@ -7,7 +7,7 @@ export function CoverImage() {
   return (
     <AspectRatio
       ratio={9 / 16}
-      className="md:mr-50 max-w-[1104px] bg-dracula-aro-50 gradient-mask-b-10 dark:bg-dracula-aro-900 dark:gradient-mask-t-50 md:-mt-36 lg:mr-56"
+      className="md:mr-50 max-w-[1104px] bg-dracula-aro-50 dark:bg-dracula-aro-900 md:-mt-36 lg:mr-56"
     >
       <Image
         src={sin_cabeza}
