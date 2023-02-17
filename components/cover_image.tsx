c7cd9ecf2e49d46831705704c4237f66f0404f54
Miguel Galperin <miguel.galperin@gmail.com>
2023-02-17 15:36:20 -0300

    tablet responsive

--- components/cover_image.tsx
+++ components/cover_image.tsx
@@ -7,21 +7,33 @@ import sin_cabeza from "../public/ipad_new.png"
 export function CoverImage() {
   return (
     <>
-      <AspectRatio ratio={9 / 16} className="hidden p-0 md:block">
-        <Image
-          src={sin_cabeza}
-          alt="Foto de Galperin"
-          className="mt-10 rounded-md object-cover transition-all duration-500 ease-in-out md:ml-[110px] lg:ml-[160px] xl:ml-[280px]"
-        />
-      </AspectRatio>
+      <div className="flex h-full items-center justify-center md:mt-10 md:ml-[110px] lg:ml-[160px] xl:ml-[280px]">
+        <AspectRatio ratio={16 / 9} className="hidden p-0 md:block">
+          <Image
+            src={sin_cabeza}
+            alt="Foto de Galperin"
+            className="mt-12 antialiased transition-all duration-500 ease-in-out first-line:rounded-md"
+            fill
+          />
+        </AspectRatio>
+      </div>
       <div className="md:hidden">
         <Image
           src={sin_cabeza_mobile}
           alt="Foto de Galperin"
           fill
-          className="mt-10 rounded-md transition-all duration-500 ease-in-out"
+          className="mt-10 rounded-md antialiased transition-all duration-500 ease-in-out"
         />
       </div>
+      <style jsx>{`
+        @media (min-height: 768px) and (orientation: portrait) {
+          .md\:mt-10 {
+            margin-top: 30%;
+            transform: scale(1.5);
+            margin-left: 20%;
+          }
+        }
+      `}</style>
     </>
   )
 }
