39be5b51d57d07a3e08d2b53647abfe61bb7d396
Miguel Galperin <miguel.galperin@gmail.com>
2023-02-17 13:01:19 -0300

    cover image new responsive

--- components/cover_image.tsx
+++ components/cover_image.tsx
@@ -1,16 +1,27 @@
 import Image from "next/image"
 
 import { AspectRatio } from "@/components/ui/aspect-ratio"
+import sin_cabeza_mobile from "../public/avatar.png"
 import sin_cabeza from "../public/ipad_new.png"
 
 export function CoverImage() {
   return (
-    <AspectRatio ratio={9 / 16} className="">
-      <Image
-        src={sin_cabeza}
-        alt="Foto de Galperin"
-        className="mt-10 rounded-md object-cover transition-all duration-500 ease-in-out md:ml-[120px] lg:ml-[180px] xl:ml-[280px]"
-      />
-    </AspectRatio>
+    <>
+      <AspectRatio ratio={9 / 16} className="hidden p-0 md:block">
+        <Image
+          src={sin_cabeza}
+          alt="Foto de Galperin"
+          className="mt-10 rounded-md object-cover transition-all duration-500 ease-in-out md:ml-[110px] lg:ml-[160px] xl:ml-[280px]"
+        />
+      </AspectRatio>
+      <div className="md:hidden">
+        <Image
+          src={sin_cabeza_mobile}
+          alt="Foto de Galperin"
+          fill
+          className="rounded-md transition-all duration-500 ease-in-out"
+        />
+      </div>
+    </>
   )
 }
