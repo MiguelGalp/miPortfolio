4a25c615febdf7a4933df378ed6e2194d2a0ba30
MiguelGalp <miguel.galperin@gmail.com>
2023-02-21 19:37:15 -0300

    new hero 2

---
+++ components/contact_principal.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Icons } from "./icons";
+import { CheckCircleIcon } from "lucide-react";
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm();
+  const [successMessage, setSuccessMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function onSubmit(data) {
+    axios
+      .post("https://eokw4aiw1ww0m93.m.pipedream.net", data)
+      .then((response) => {
+        setSuccessMessage(
+          `Gracias por tu contacto! Chequeá tu correo para continuar la charla 😊`
+        );
+        setSubmitted(true);
+      })
+      .catch((e) => console.error(e));
+  }
+
+  if (submitted) {
+    return (
+      <div className="flex items-center space-x-2 bg-green-100 p-2 rounded-md">
+        <CheckCircleIcon className="h-6 w-6 text-green-600" />
+        <p className="text-green-800">{successMessage}</p>
+      </div>
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input {...register("email")} defaultValue="me@gmail.com"></Input>
+        <Button type="submit" role="submit">
+          {isSubmitting ? "Submitting" : "Contacto"}
+        </Button>
+      </div>
+    </form>
+  );
+}
--- components/cover_image.tsx
+++ components/cover_image.tsx
@@ -1,30 +1,105 @@
 import Image from "next/image"
+import Link from "next/link"
 
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import smallImage from "../public/smallImage.png"
 import mediumImage from "../public/mediumImage.png"
 import largeImage from "../public/coderGIF.svg"
 import extraLargeImage from "../public/extraLargeImage.png"
+import { Button } from "./ui/button"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+
+
+
+import { CalendarDays } from "lucide-react"
+ 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter
+} from "@/components/ui/dialog"
 
 export function CoverImage() {
   return (
-    <div className="flex min-h-screen bg-transparent relative bottom-[100px]">
-  <div className="flex flex-1 flex-col justify-center px-8 py-8 md:px-12 lg:flex-none lg:px-24">
+    <div className="flex min-h-screen bg-transparent relative bottom-[80px]">
+  <div className="flex flex-1 flex-col justify-center px-8 py-8 md:px-12 lg:flex-none lg:px-24 lg:py-36">
     <div className="mx-auto w-full lg:max-w-6xl">
-      <div className="mx-auto max-w-xl text-center lg:p-10 lg:text-left">
+      <div className="mx-auto max-w-xl text-center lg:p-1 lg:text-left">
         <div>
           <span className="inline-flex items-center text-black"> <span className="font-mono text-sm" aria-hidden="true">MG</span><span className="ml-3 h-3.5 w-px bg-black"></span><span className="ml-3 text-base font-medium tracking-tight">Desarrollador</span> </span>
-          <p className="font-display mt-8 text-4xl tracking-tighter text-black lg:text-5xl">Apps y webs, juntos</p>
-          <p className="mt-4 max-w-xl text-lg tracking-tight text-gray-600">Escribime para pensar tu nueva herramienta. La hago en React, Next.js, Tailwind y en contextos que piensan en el usuario y las APIs.</p>
+          <p className="font-display mt-8 text-4xl tracking-tighter text-black lg:text-5xl">Cultura digital, juntos</p>
+          <p className="mt-4 max-w-xl text-lg tracking-tight text-gray-600">Me especializo en dar vida a aplicaciones y sitios web centrados en el usuario. Desde frontends hasta plataformas micro SaaS, podemos hacer realidad tu visión utilizando 
+          <HoverCard>
+      <HoverCardTrigger asChild>
+        <Link href="/"> Next.js</Link>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-80">
+        <div className="flex justify-between space-x-4">
+          <Avatar>
+            <AvatarImage src="https://github.com/vercel.png" />
+            <AvatarFallback>VC</AvatarFallback>
+          </Avatar>
+          <div className="space-y-1">
+            <h4 className="text-sm font-semibold">@nextjs</h4>
+            <p className="text-sm">
+              The React Framework – created and maintained by @vercel.
+            </p>
+          </div>
+        </div>
+      </HoverCardContent>
+    </HoverCard>, Pipedream y Tailwind. Trabajemos juntos para construir la herramienta que querés. Escribime hoy para discutir tu proyecto!</p>
         </div>
         <div className="mt-10 flex flex-col gap-3 sm:flex-row">
+        <Dialog>
+  <DialogTrigger>
+    <Button>Contacto</Button>
+  </DialogTrigger>
+  <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+</Dialog>
         </div>
       </div>
     </div>
   </div>
   <div className="relative order-first hidden w-0 flex-1 bg-transparent lg:block">
     <div>
-      <img className="absolute inset-0 h-full w-full object-cover scale-75" src="/coderGIF.svg" alt="" />
+      <img className="absolute bottom-5 md:-ml-[50px] lg:ml-24 xl:ml-[100px] h-full w-full object-cover scale-[0.7]" src="/coderGIF.svg" alt="" />
     </div>
   </div>
 </div>
