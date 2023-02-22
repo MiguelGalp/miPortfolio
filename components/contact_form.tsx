5c285d8b2c32f3f53dfa7b2437e0a068b4ad5b13
Miguel Galperin <miguel.galperin@gmail.com>
2023-02-22 17:28:51 -0300

    correction

--- components/contact_form.tsx
+++ components/contact_form.tsx
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Icons } from "./icons";
-import { CheckCircleIcon } from "lucide-react";
+import React, { useState } from "react"
+import axios from "axios"
+import { CheckCircleIcon } from "lucide-react"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Icons } from "./icons"
 
 export default function ContactForm() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm();
-  const [successMessage, setSuccessMessage] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  } = useForm()
+  const [successMessage, setSuccessMessage] = useState("")
+  const [submitted, setSubmitted] = useState(false)
 
   function onSubmit(data) {
     axios
@@ -21,29 +22,29 @@ export default function ContactForm() {
       .then((response) => {
         setSuccessMessage(
           `Gracias por tu contacto! Chequeá tu correo para continuar la charla 😊`
-        );
-        setSubmitted(true);
+        )
+        setSubmitted(true)
       })
-      .catch((e) => console.error(e));
+      .catch((e) => console.error(e))
   }
 
   if (submitted) {
     return (
-      <div className="flex items-center space-x-2 bg-green-100 p-2 rounded-md">
+      <div className="flex items-center space-x-2 rounded-md bg-green-100 p-2">
         <CheckCircleIcon className="h-6 w-6 text-green-600" />
         <p className="text-green-800">{successMessage}</p>
       </div>
-    );
+    )
   }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex w-full max-w-sm justify-content-start space-x-2">
+      <div className="justify-content-start flex w-full max-w-sm space-x-2">
         <Input {...register("email")} defaultValue="Tu email"></Input>
         <Button type="submit" role="submit">
           {isSubmitting ? "Submitting" : "Enviar"}
         </Button>
       </div>
     </form>
-  );
+  )
 }
