74b4ae1012e1bc7b874f099826479ab9b0d04f94
MiguelGalp <miguel.galperin@gmail.com>
2023-02-25 14:24:05 -0300

    new image size

--- components/imagecomponent.tsx
+++ components/imagecomponent.tsx
@@ -5,7 +5,7 @@ const ImageComponent = () => {
   return (
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
       <Image
-        src="/love_hero.png"
+        src="/love_hero_2.png"
         alt="Love hero image"
         layout="fill"
         objectFit="cover"
