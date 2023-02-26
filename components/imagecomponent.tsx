4e9242ebe8c68d4ba5af0d17762120210983b216
MiguelGalp <miguel.galperin@gmail.com>
2023-02-26 12:33:38 -0300

    Spinner

--- components/imagecomponent.tsx
+++ components/imagecomponent.tsx
@@ -1,19 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
+import { ClipLoader } from "react-spinners"
 
 const ImageComponent = () => {
+  const [loading, setLoading] = useState(true)
+
+  const handleOnLoad = () => {
+    setLoading(false)
+  }
+
   return (
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
+      {loading && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <ClipLoader />
+        </div>
+      )}
       <Image
-        src="/love_hero_2.png"
+        src="/love_hero.png"
         alt="Love hero image"
         layout="fill"
         objectFit="cover"
         className="rounded-md"
-        loading="eager"
+        loading="lazy"
+        onLoad={handleOnLoad}
       />
     </div>
   )
 }
 
 export default ImageComponent
+
