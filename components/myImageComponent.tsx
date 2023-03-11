0f0b9a005e70fb5f4aa70f2ceb695097067fa9eb
MiguelGalp <miguel.galperin@gmail.com>
2023-03-11 12:39:06 -0300

    newer cover

--- components/myImageComponent.tsx
+++ components/myImageComponent.tsx
@@ -9,7 +9,7 @@ function MyImageComponent() {
       width={500}
       height={500}
       className="absolute bottom-5 left-8 top-5 h-full w-full scale-[730%] object-contain"
-      src="/Glyphy.png"
+      src="/Wtf_Migue.png"
       alt=""
       priority
     />
@@ -22,7 +22,7 @@ function MyDarkImageComponent() {
       width={500}
       height={500}
       className="absolute bottom-5 left-8 top-5 h-full w-full scale-[730%] object-contain"
-      src="/Glyphy.png"
+      src="/Wtf_Migue.png"
       alt=""
       priority
     />
