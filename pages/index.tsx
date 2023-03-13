f978c88f9219c2184f00ab92513ac557868f13dd
MiguelGalp <miguel.galperin@gmail.com>
2023-03-13 14:29:55 -0300

    correct favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function IndexPage() {
           content="Portfolio de Software de Miguel Galperin"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon_2.ico" />
       </Head>
       <CoverImage />
     </Layout>
