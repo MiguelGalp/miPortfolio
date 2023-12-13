8d8b8e50287c3962823fe3cce7636369613b1570
MiguelGalp <miguel.galperin@gmail.com>
2023-12-13 18:22:45 -0300

    MobileNew

--- components/responsive_cover.tsx
+++ components/responsive_cover.tsx
@@ -3,26 +3,26 @@ import React from 'react';
 
 const ResponsiveCover = () => {
     return (
-        <div className="grid grid-cols-3 gap-4">
-            <div className="col-span-2">
-                <img src="/coderGIF.svg" alt="placeholder" className="w-full h-full object-cover -mt-4" />
+        <div className="gap-4 grid-cols-1 flex flex-col items-center">
+            <div className="col-span-2 sm:flex-shrink-0 -mt-10">
+                <img src="/coderGIF.svg" alt="placeholder" className="w-full h-full object-cover sm:mt-0" />
             </div>
-            <div className="col-span-1 flex flex-col justify-center -ml-3 space-y-4">
-                <div className="flex items-center mt-8">
-                    <h2 className="text-sm font-semibold">MG</h2>
-                    <Separator className="mx-2" orientation="vertical" />
-                    <h2 className="text-sm font-semibold">Desarrollador</h2>
+            <div className="col-span-1 flex flex-col justify-center space-y-4 ml-6">
+                <div className="flex items-center ml-1">
+                    <h2 className="font-muted sm:text-base">MG</h2>
+                    ⎟
+                    <h2 className="text-sm font-muted sm:text-base">Desarrollador</h2>
                 </div>
-                <h1 className="text-5xl tracking-tighter">CULTURA DIGITAL, JUNTOS.</h1>
-                <h3 className="mr-4">Me especializo en dar vida a aplicaciones y sitios web centrados en el usuario. Desde frontends hasta plataformas de micro-servicios digitales, te puedo ayudar a hacer realidad tu visión. </h3>
+                <h1 className="text-5xl tracking-tighter font-bold relative bottom-3">CULTURA DIGITAL, JUNTOS.</h1>
+                <h3 className="mr-8 text-2xl ml-1 tracking-tight leading-6">Me especializo en dar vida a aplicaciones y sitios web centrados en el usuario. Desde frontends hasta plataformas de micro-servicios digitales, te puedo ayudar a hacer realidad tu visión. </h3>
             </div>
 
-            <div className="col-span-2 mx-6 text-xs -mt-12">
+            <div className="col-span-2 mx-7 text-base leading-5">
                 <p>Utilizo un Stack moderno y económico (Next.js y Tailwind en el cliente, Python en el servidor). Trabajemos juntos para construir la herramienta que querés. Escribime hoy para discutir tu proyecto!</p>
             </div>
-            <div className="col-span-1 flex items-start justify-left">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded -ml-3">
-                    Call To Action
+            <div className="col-span-1 flex items-start justify-left sm:col-span-1 sm:flex items-center justify-center py-6">
+                <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-6 rounded -ml-3 sm:ml-0">
+                    Contacto
                 </button>
             </div>
         </div>
