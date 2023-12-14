92a89239dee92e5cb74a5f5342ac8a3a02c19f49
MiguelGalp <miguel.galperin@gmail.com>
2023-12-14 20:22:40 -0300

    hero_mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,7 +11,7 @@ export function SiteFooter() {
     <footer className="relative w-full lg:bottom-[15vh] md:top-[10vh] ">
       <div className="flex flex-col items-center justify-center gap-4 md:mt-2 md:h-24 md:flex-row">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Copyright className="h-6 w-6 md:h-4" />
+          <Copyright className="h-6 w-6 md:h-4"/>
           <p className="text-center text-[10px] leading-normal text-slate-900 dark:text-slate-100 md:text-left md:text-sm">
             Portfolio de software de{" "}
             <a
--- components/responsive_cover.tsx
+++ components/responsive_cover.tsx
@@ -3,24 +3,24 @@ import React from 'react';
 
 const ResponsiveCover = () => {
     return (
-        <div className="gap-4 grid-cols-1 flex flex-col items-center">
-            <div className="col-span-2 sm:flex-shrink-0 -mt-10">
-                <img src="/coderGIF.svg" alt="placeholder" className="w-full h-full object-cover sm:mt-0" />
+        <div className="gap-4 grid-cols-1 flex flex-col ">
+            <div className="col-span-2 ">
+                <img src="/Cortado.png" alt="placeholder" className="w-full h-full object-cover" />
             </div>
             <div className="col-span-1 flex flex-col justify-center space-y-4 ml-6">
-                <div className="flex items-center ml-1">
+                <div className="flex items-center">
                     <h2 className="font-muted sm:text-base">MG</h2>
                     ⎟
                     <h2 className="text-sm font-muted sm:text-base">Desarrollador</h2>
                 </div>
-                <h1 className="text-5xl tracking-tighter font-bold relative bottom-3">CULTURA DIGITAL, JUNTOS.</h1>
-                <h3 className="mr-8 text-2xl ml-1 tracking-tight leading-6">Me especializo en dar vida a aplicaciones y sitios web centrados en el usuario. Desde frontends hasta plataformas de micro-servicios digitales, te puedo ayudar a hacer realidad tu visión. </h3>
+                <h1 className="text-5xl tracking-tighter font-bold relative bottom-3 -ml-1">CULTURA DIGITAL, JUNTOS.</h1>
+                <h3 className="mr-8 text-2xl tracking-tight leading-6">Apps, datos, contenidos y servicios para el sector creativo.</h3>
             </div>
 
-            <div className="col-span-2 mx-7 text-base leading-5">
+            <div className="col-span-2 mx-6 text-base leading-5">
                 <p>Utilizo un Stack moderno y económico (Next.js y Tailwind en el cliente, Python en el servidor). Trabajemos juntos para construir la herramienta que querés. Escribime hoy para discutir tu proyecto!</p>
             </div>
-            <div className="col-span-1 flex items-start justify-left sm:col-span-1 sm:flex justify-center py-6">
+            <div className="col-span-1 flex items-start justify-left sm:col-span-1 sm:flex justify-start py-6 px-9">
                 <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-6 rounded -ml-3 sm:ml-0">
                     Contacto
                 </button>
