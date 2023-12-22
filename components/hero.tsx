f9dd49b0aad01deba6cfc74fdb9cb9479561c21b
MiguelGalp <miguel.galperin@gmail.com>
2023-12-22 16:24:10 -0300

    Suspense

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,14 +1,19 @@
-"user client"
-import { useMediaQuery } from 'react-responsive'
-import CoverImage from './cover_image'
-import ResponsiveCover from './responsive_cover'
+"use client";
+import { Suspense } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import CoverImage from './cover_image';
+import ResponsiveCover from './responsive_cover';
+import { ClipLoader } from 'react-spinners';
 
 export default function Hero() {
-  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1024 })
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1024 });
 
   return (
     <div>
-      {isDesktopOrLaptop ? <CoverImage /> : <ResponsiveCover />}
+      <Suspense fallback={<div><ClipLoader/></div>}>
+        {isDesktopOrLaptop ? <CoverImage /> : <ResponsiveCover />}
+      </Suspense>
     </div>
-  )
+  );
 }
+
